afef1d58615cb84f4041f7c99aa3c312ea20ce34
Your Name <you@example.com>
2018-04-21 15:29:02 +0700

    Akhirnya bisa juga

---
+++ Brezyn/App.js
@@ -0,0 +1,49 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label } from 'native-base';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Container>
+        <Content>
+        <View>
+        <Image
+          style={{width: 50, height: 50}}
+          source={require('./assets/google.png')}
+        />
+        </View>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
+}
